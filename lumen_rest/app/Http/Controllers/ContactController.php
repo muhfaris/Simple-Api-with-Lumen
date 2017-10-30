a5ff7603e7c81e7c198826df1a983d4d3d6c3ff4
Faris Afif <maillfaris@gmail.com>
2017-10-30 17:27:26 +0700

    CRUD Controller

--- lumen_rest/app/Http/Controllers/ContactController.php
+++ lumen_rest/app/Http/Controllers/ContactController.php
@@ -15,10 +15,34 @@ class ContactController extends Controller {
   
     }
 
-    public function getcontact($id) {
+    public function getContact($id) {
         $Contact  = Contact::find($id);  
         return response()->json($Contact);
     }
+   
+    public function createContact(Request $request){
+    	$Contact = Contact::create($request->all());
+    	return response()->json($Contact);
+ 
+    }
+
+    public function updateContact(Request $request, $id){
+
+    	$Contact  = Contact::find($id);
+    	$Contact->name = $request->input('name');
+    	$Contact->email = $request->input('email');
+    	$Contact->save();
+
+    	return response()->json($Contact);
+	}
+
+    public function deleteContact($id){
+    	$Contact  = Contact::find($id);
+    	$Contact->delete();
+ 
+    	return response()->json('Removed successfully.');
+	}
+
 
 
 }
