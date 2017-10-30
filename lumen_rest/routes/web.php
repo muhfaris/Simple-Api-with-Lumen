aab6414425ed7397021a0b5f9a3051ab969085ee
Faris Afif <maillfaris@gmail.com>
2017-10-30 17:27:41 +0700

    CRUD routing

--- lumen_rest/routes/web.php
+++ lumen_rest/routes/web.php
@@ -2,10 +2,10 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| $routerlication Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
+| Here is where you can register all of the routes for an $routerlication.
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
 |
@@ -16,8 +16,10 @@ $router->get('/api', function () use ($router) {
 });
 
 $router->get('api/v1/contact','ContactController@index');
-$router->get('api/v1/contact/{id}','ContactController@getcontact'); 
-
+$router->get('api/v1/contact/{id}','ContactController@getContact'); 
+$router->post('api/v1/contact','ContactController@createContact');
+$router->put('api/v1/contact/{id}','ContactController@updateContact');
+$router->delete('api/v1/contact/{id}','ContactController@deleteContact'); 
 
 
  
